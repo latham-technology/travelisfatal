62ccd88ffa58fa3f5e732d394c54a6f35581b82b
Matt Latham <github@mattlatham.dev>
2024-03-31 12:38:30 -0700

    adds analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,7 @@ export default defineNuxtConfig({
     },
 
     modules: [
+        '@nuxtjs/plausible',
         '@nuxtjs/tailwindcss',
         '@nuxtjs/google-fonts',
         '@nuxtjs/cloudinary',
