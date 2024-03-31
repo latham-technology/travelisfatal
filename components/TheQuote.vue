2a803cbb896e7c35718bb63646b77901a4bbb0e9
Matt Latham <github@mattlatham.dev>
2024-03-31 03:19:55 -0700

    nuxt3

---
+++ components/TheQuote.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="text-3xl lg:text-5xl font-bold flex flex-col p-8">
+        <h1>
+            “Travel is fatal to prejudice, bigotry, and narrow-mindedness, and many of our people need it sorely on these accounts.
+            <br /><br/>
+            Broad, wholesome, charitable views of men and things cannot be acquired by vegetating in one little corner of the earth all one's lifetime.”
+        </h1>
+        <span class="ml-auto mt-8">― Mark Twain</span>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+div {
+    font-family: 'Forum';
+}
+</style>
