fcd4a7e7d055bdd882fcd08d4d167d2def5f95f8
Matt Latham <github@mattlatham.dev>
2024-04-06 16:21:29 -0700

    fix spelling

--- composables/useSeo.js
+++ composables/useSeo.js
@@ -1,10 +1,13 @@
 export default () => {
     useSeoMeta({
-        title: 'Photography by Francis Latham',
-        ogTitle: 'Photography by Francis Latham',
-        description: 'Photography gallery of China, Syria, Jordan, and Costa Rica circa 1995 by Francis Latham',
-        ogDescription: 'Photography gallery of China, Syria, Jordan, and Costa Rica circa 1995 by Francis Latham',
-        ogImage: 'https://res.cloudinary.com/mattlatham/image/upload/w_auto/v1/franlatham/China/img152_aloseu',
-        twitterCard: 'summary_large_image'
+        title: 'Photography by Frances Latham',
+        ogTitle: 'Photography by Frances Latham',
+        description:
+            'Photography gallery of China, Syria, Jordan, and Costa Rica circa 1995 by Frances Latham',
+        ogDescription:
+            'Photography gallery of China, Syria, Jordan, and Costa Rica circa 1995 by Frances Latham',
+        ogImage:
+            'https://res.cloudinary.com/mattlatham/image/upload/w_auto/v1/franlatham/China/img152_aloseu',
+        twitterCard: 'summary_large_image',
     })
 }
